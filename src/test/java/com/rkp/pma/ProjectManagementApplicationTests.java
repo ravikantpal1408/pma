fa2a7b6a8ecc7010ad3154ec198231c5530d9af2
Ravi Pal <ravipal@Ravis-MacBook-Air.local>
2020-10-08 21:10:09 +0530

    wrote one assertion in jUnit

--- src/test/java/com/rkp/pma/ProjectManagementApplicationTests.java
+++ src/test/java/com/rkp/pma/ProjectManagementApplicationTests.java
@@ -1,13 +1,25 @@
 package com.rkp.pma;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 @SpringBootTest
 class ProjectManagementApplicationTests {
 
+	// @BeforeTestClass
+	// public void beforeClass() {
+	// 	System.out.println("in before class");
+	// }
+
 	@Test
 	void contextLoads() {
+		String valueOne = "test";
+		String valueTwo = "test";
+		assertEquals(valueOne, valueTwo);
 	}
+
 
 }
