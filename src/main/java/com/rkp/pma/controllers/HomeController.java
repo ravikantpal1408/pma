f987e919238a0f50f48d3ea188e8e242ccf69fa3
Ravi Pal <ravipal@Ravis-MacBook-Air.local>
2020-10-10 01:26:21 +0530

    added some cofiguration

--- src/main/java/com/rkp/pma/controllers/HomeController.java
+++ src/main/java/com/rkp/pma/controllers/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import com.rkp.pma.entities.Project;
 @Controller
 public class HomeController {
 
+	@Value("${version}")
+	private String ver;
+
 	@Autowired
 	IProjectRepository projRepo;
 
@@ -29,7 +33,8 @@ public class HomeController {
 	@GetMapping("/")
 	public String displayHome(Model model) throws JsonProcessingException {
 
-		Map<String, Object> map = new HashMap<>();
+		// Map<String, Object> map = new HashMap<>();
+		model.addAttribute("versionNumber", ver);
 
 		List<Project> obj = projRepo.findAll();
 		model.addAttribute("projects", obj);
